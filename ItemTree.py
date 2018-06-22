256fdfa505914cf7b1ec0608e86632bfbd120ef1
Jordi Carrera Ventura <jordi.carrera.ventura@gmail.com>
2018-06-22 09:48:26 +0200

    Added parameter max_size; fixed bug when piping Unicode to a file from stdout.

--- ItemTree.py
+++ ItemTree.py
@@ -18,6 +18,7 @@ HELP_MIN_FREQ = 'Minimum frequency of any item to be used as a node in the tree.
 HELP_PROB = 'Maximum probability of any item to be used as a node in the tree.'
 HELP_MIN_PROB = 'Minimum probability of any item to be used as a node in the tree.'
 HELP_SIZE = 'Minimum size for a tree branch to be returned.'
+HELP_MAXSIZE = 'Maximum size for a tree branch to be returned.'
 HELP_FORMAT = """Output format:
 \'xy\' = input strings + item trees;
 \'yx\' = item trees + input strings;
@@ -42,6 +43,7 @@ class ItemTree:
     def __init__(
         self,
         min_size=1,
+        max_size=1,
         max_freq=0.5,
         min_freq=2,
         sorted=True,
@@ -49,6 +51,7 @@ class ItemTree:
         itemtext=False
     ):
         self.min_size = min_size
+        self.max_size = max_size
         self.min_freq = min_freq
         self.max_freq = max_freq
         self.sorted = sorted
@@ -73,8 +76,18 @@ class ItemTree:
         elif len(X) <= self.min_size:
             return [(False, history, X)]
         features_to_deduct = set(history)
-        F, I = self.__count(X, features_to_deduct)
-        return self.__divide(cluster, F, I)
+        over_max_size = True
+        while over_max_size:
+            F, I = self.__count(X, features_to_deduct)
+            bf, clusters = self.__divide(cluster, F, I)
+            if not bf:
+                over_max_size = False
+            elif len(clusters[0]) > self.max_size \
+            or len(clusters[1]) > self.max_size:
+                features_to_deduct.add(bf)
+            else:
+                over_max_size = False
+        return clusters
     
     def __count(self, X, features_to_deduct):
         F = Counter()
@@ -102,9 +115,9 @@ class ItemTree:
             break
         if best_feat:
             a, b = self.__a_or_b(history, X, I, best_feat)
-            return [a, b]
+            return best_feat, [a, b]
         else:
-            return [(False, history, X)]
+            return None, [(False, history, X)]
     
     def __a_or_b(self, history, X, I, best_feat):
         aX = []
@@ -184,9 +197,11 @@ class ItemTree:
 
 
 def config_display(args):
-    return '<ItemTree input_file="%s" <max_freq=%d max_prob=%f min_freq=%d min_prob=%f min_size=%d> <output=%s nosort=%s preproc=%s itemtext=%s>>\n' % (
-        args.input_file, args.max_freq, args.max_prob, args.min_freq, args.min_prob,
-        args.min_size, args.output, args.nosort, args.preproc,
+    return '<ItemTree input_file=%s <max_freq=%d max_prob=%f min_freq=%d min_prob=%f min_size=%d max_size=%d> <output=%s nosort=%s preproc=%s itemtext=%s>>\n' % (
+        args.input_file,
+        args.max_freq, args.max_prob, args.min_freq, args.min_prob,
+        args.min_size, args.max_size,
+        args.output, args.nosort, args.preproc,
         args.itemtext
     )
 
@@ -216,9 +231,14 @@ def get_args():
         help=HELP_MIN_PROB
     )
     parser.add_argument(
-        '-s', '--min_size',
+        '--min_size',
         type=int, default=2,
         help=HELP_SIZE
+    )
+    parser.add_argument(
+        '--max_size',
+        type=int, default=50,
+        help=HELP_MAXSIZE
     )
     parser.add_argument(
         '--output',
@@ -319,7 +339,8 @@ def write(args, clusters):
     
     def display(clusters):
         for cl in clusters:
-            sys.stdout.write('%s\n' % "\t".join(cl))
+            cl_txt = [cl[0].encode('utf-8'), cl[1].encode('utf-8')]
+            sys.stdout.write('%s\n' % "\t".join(cl_txt))
 
     if args.output_file:
         save(clusters, args.output_file)
@@ -339,6 +360,7 @@ if __name__ == '__main__':
         max_freq=maxim,
         min_freq=minim,
         min_size=args.min_size,
+        max_size=args.max_size,
         sorted=not args.nosort,
         format=args.output,
         itemtext=args.itemtext
